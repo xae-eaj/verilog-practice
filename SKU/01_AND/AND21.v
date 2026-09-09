88fc9d8db7e29c13baab2f33c88f348d4079fc1b
SeungJae <255730974+xae-eaj@users.noreply.github.com>
2026-09-09 12:04:06 +0900

    Delete SKU directory

--- SKU/01_AND/AND21.v
+++
@@ -1,7 +0,0 @@
-module AND21(a, b, y);
-    input a, b;
-    output y;
-
-    and (y, a, b);
-
-endmodule
--- SKU/01_OR/OR21.v
+++
@@ -1,7 +0,0 @@
-module OR21(a, b, y);
-    input a, b;
-    output y;
-
-    or (y, a, b);
-
-endmodule
